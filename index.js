8e8c11e7a4480ac0016f67ed2702ec220597c547
RenYuXiang <mail@yuxiang.ren>
2018-09-12 09:58:30 +0800

    update

--- index.js
+++ index.js
@@ -10,24 +10,55 @@ let page = new Router()
 page.get('/404', async ( ctx )=>{
     ctx.body = '404 page!'
 }).get('/generateKey', async ( ctx )=>{
+
     account = ctx.query.account;
-    key = genKey.uuid.generateUUID();
-    b32 = genKey.uuid.base32(key);
-    qrcodeUrl = 'http://qrcode.kaywa.com/img.php?s=8&d=' + encodeURIComponent('otpauth://totp/'+account+'?secret=' + b32);
-    qrcodeStr = encodeURIComponent('otpauth://totp/'+account+'?secret=' + b32);
-    ctx.body = {"secret_key":key,"base32":b32,"qrcodeUrl":qrcodeUrl,"qrcodeStr":qrcodeStr};
+    if ( !checkHost(ctx) || account == undefined || account == null || account.length == 0) {
+        ctx.body = {
+            "result": "request error!",
+            "context": ctx
+        };
+    }else{
+        key = genKey.uuid.generateUUID();
+        b32 = genKey.uuid.base32(key);
+        qrcodeStr = encodeURIComponent('otpauth://totp/'+account+'?secret=' + b32);
+        qrcodeUrl = 'http://qrcode.kaywa.com/img.php?s=8&d=' + qrcodeStr;
+       
+        ctx.body = {
+            "secret_key":key,
+            "base32":b32,
+            "qrcodeUrl":qrcodeUrl,
+            "qrcodeStr":qrcodeStr
+        };
+    }
 }).get('/verify',async(ctx)=>{
+
+    if(!checkHost(ctx)) {
+        ctx.body = {
+            "result": "request error!",
+            "context": ctx
+        };
+        return;
+    }
     param = ctx.query;
-    ctx.body = {"result":otp.totp.verify(param.code,param.key),"code":param.code,"key":param.key}
+    ctx.body = {
+        "result":otp.totp.verify(param.code,param.key),
+        "code": param.code,
+        "key": param.key
+    };
 })
 
 // Deal with CORS's problem
 app.use(cors())
 
-let router = new Router()
-router.use('/api', page.routes(), page.allowedMethods())
-app.use(router.routes()).use(router.allowedMethods())
+// let router = new Router()
+// router.use('/api', page.routes(), page.allowedMethods())
+app.use(page.routes()).use(page.allowedMethods())
 
 app.listen(3000, () => {
   console.log('[demo] route-use-middleware is starting at port 3000')
 })
+
+function checkHost(ctx) {
+    host = ctx.request.header.host;
+    return host.indexOf('localhost') != -1 || host.indexOf('0.0.0.0') != -1 || host.indexOf('127.0.0.1') != -1;
+}
